514a8d0f75314dd5380720c1ebf554df53ae0b3f
Itachi <github@itachi.pl>
2024-12-08 18:55:20 +0100

    Implement the OAuth flow.
    This is a very first approach. It works, but it's not beautiful yet.

--- nodes/spotify-api.js
+++ nodes/spotify-api.js
@@ -1,7 +1,53 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+
 module.exports = function (RED) {
+  let tokenMap = {};
+
   function RemoteServerNode(config) {
     RED.nodes.createNode(this, config);
   }
+
+  RED.httpAdmin.get("/spotify/oauth", (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+
+    if (state !== null && code !== null) {
+      tokenMap[state] = code;
+    }
+
+    res.sendStatus(200);
+  });
+
+  RED.httpAdmin.get("/spotify/oauth/continue", (req, res) => {
+    const state = req.query.state || null;
+    const secret = req.query.secret || null;
+    const clientId = req.query.client_id || null;
+
+    if (secret == null || clientId == null || state == null) {
+      res.sendStatus(400);
+    } else if (!(state in tokenMap)) {
+      res.sendStatus(202);
+    } else {
+      const code = tokenMap[state];
+      const credentials = {
+        clientId: clientId,
+        clientSecret: secret,
+        redirectUri: "http://localhost:1880/spotify/oauth"
+      };
+      const spotifyApi = new SpotifyWebApi(credentials);
+
+      spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+          access_token: data.body["access_token"],
+          refresh_token: data.body["refresh_token"]
+        });
+      }).catch(err => {
+        res.status(500).send(err.message);
+      }).finally(() => {
+        tokenMap = {};
+      });
+    }
+  })
 
   RED.nodes.registerType("spotify-api", RemoteServerNode, {
     credentials: {
