475a4cefb3f169f08a27badf9d77b50550a7e2c0
Krzysztof Bąk <github@itachi.pl>
2024-12-19 11:18:12 +0100

    Improve authentication process (#2)
    Keep the refresh token in a custom credentials storage. Thanks to this, whenever the refresh token changes (which happens from time to time), it's remembered in the persistent storage, so it can sustain Node-RED restarts.
    
    Force user to save and deploy the configuration node before triggering OAuth flow. Thanks to this, the secret is safely stored inside Node-RED configuration, and it's a less chance for it to leak.
    
    The OAuth's redirect page has been slightly improved, so it doesn't look like a scam (that much).
    
    Committed changes
    -----------------
    
    * Small improvements.
    
    * Idea project configuration.
    
    * Implement a helper lib for storing encrypted credentials.
    
    This is required in order to periodically store new refresh tokens. Sadly, Node-RED doesn't allow to programmatically update credentials, so it must be done on our own.
    
    * Add debugging helpers to Dockerfile.
    
    * Store refresh token in the custom credentials storage.
    
    * Store refresh token in the custom credentials storage.
    
    * Read the refresh token from the custom credentials store.
    
    When the node has been removed, also remove the file with credentials.
    
    * Make the redirect page prettier, and move it to a separate file.
    
    * Add hidden form field to track authentication progress.
    
    The form field is required, and is set when the OAuth flow completes. This way, the configuration node can be shown as invalid until the user finishes the whole OAuth flow.

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,15 @@ FROM nodered/node-red:4.0.5-22-minimal
 
 COPY . /tmp/node-red-spotify
 RUN npm install /tmp/node-red-spotify
+
+# Debugging section
+#
+# 1. Bind port 9229 to localhost
+# 2. Uncomment entrypoint:
+# ENTRYPOINT ["npm"]
+#
+# 3a. Uncomment below CMD for breaking immediately after Node-RED start.
+# CMD ["run", "debug_brk", "--", "--userDir", "/data"]
+#
+# 3b. OR uncomment below CMD for casual debugging.
+# CMD ["run", "debug", "--", "--userDir", "/data"]
--- nodes/spotify-api.js
+++ nodes/spotify-api.js
@@ -1,14 +1,54 @@
 const SpotifyWebApi = require('spotify-web-api-node');
+const credentialsStore = require("../lib/credentials.js");
+const successPageContent = require("../lib/success-page.js");
 
 module.exports = function (RED) {
   let tokenMap = {};
 
   function SpotifyApiConfig(config) {
     RED.nodes.createNode(this, config);
+    const node = this;
 
-    this.spotifyWebApi = new SpotifyWebApi();
-    this.spotifyWebApi.setAccessToken(this.credentials.access_token);
-    this.spotifyWebApi.setRefreshToken(this.credentials.refresh_token);
+    const credentials = {
+      ...node.credentials,
+      ...credentialsStore.getCredentials(node, RED)
+    }
+
+    if (credentials.refreshToken) {
+      node.spotifyWebApi = new SpotifyWebApi(credentials);
+      node.refreshTimeout = null;
+
+      refreshTokens();
+    }
+
+    node.on("close", (removed, done) => {
+      clearTimeout(node.refreshTimeout);
+
+      if (removed) {
+        credentialsStore.removeCredentials(node, RED);
+      }
+
+      done();
+    });
+
+    function refreshTokens() {
+      node.spotifyWebApi.refreshAccessToken().then(data => {
+        const {
+          access_token: accessToken,
+          expires_in: expiresIn,
+          refresh_token: refreshToken
+        } = data.body;
+
+        node.spotifyWebApi.setAccessToken(accessToken);
+
+        if (refreshToken) {
+          credentialsStore.setCredentials({refreshToken}, node, RED);
+        }
+
+        clearTimeout(node.refreshTimeout);
+        node.refreshTimeout = setTimeout(refreshTokens, expiresIn / 2 * 1000);
+      });
+    }
   }
 
   RED.httpAdmin.get("/spotify/oauth", (req, res) => {
@@ -17,35 +57,27 @@ module.exports = function (RED) {
 
     if (state !== null && code !== null) {
       tokenMap[state] = code;
+      res.send(successPageContent)
     }
-
-    res.sendStatus(200);
   });
 
   RED.httpAdmin.get("/spotify/oauth/continue", (req, res) => {
-    const state = req.query.state || null;
-    const secret = req.query.secret || null;
-    const clientId = req.query.client_id || null;
+    const {state, nodeId} = req.query;
 
-    if (secret == null || clientId == null || state == null) {
+    if (state === null || nodeId === null) {
       res.sendStatus(400);
     } else if (!(state in tokenMap)) {
       res.sendStatus(202);
     } else {
+      const node = RED.nodes.getNode(req.query.nodeId);
+      const spotifyApi = new SpotifyWebApi(node.credentials);
       const code = tokenMap[state];
-      const credentials = {
-        clientId: clientId,
-        clientSecret: secret,
-        redirectUri: "http://localhost:1880/spotify/oauth"
-      };
-      const spotifyApi = new SpotifyWebApi(credentials);
 
       spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({
-          credentials: {
-            ...data.body
-          }
-        });
+        credentialsStore.setCredentials({
+          "refreshToken": data.body.refresh_token
+        }, node, RED);
+        res.sendStatus(200);
       }).catch(err => {
         res.status(500).send(err.message);
       }).finally(() => {
@@ -56,8 +88,9 @@ module.exports = function (RED) {
 
   RED.nodes.registerType("spotify-api", SpotifyApiConfig, {
     credentials: {
-      access_token: {type: "password"},
-      refresh_token: {type: "password"},
+      clientId: {type: "text"},
+      redirectUri: {type: "text"},
+      clientSecret: {type: "password"}
     }
   });
 }
